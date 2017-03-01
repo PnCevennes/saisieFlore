a001253a53611e2d565d007aab56bec549840af8
amandine-sahl <amandine.sahl@gmail.com>
2017-03-01 11:00:23 +0100

    optimisation de la requête

--- Sources/Flore/Modeles/GeoJson/gjZpr.php
+++ Sources/Flore/Modeles/GeoJson/gjZpr.php
@@ -6,16 +6,31 @@
     require_once '../../Modeles/Filtres/fGrille.php';
     
     $cnxPgBd = new CnxPgBd();
-    $req = "SELECT st_asgeojson(st_transform(zpr_geom, 4326)), zpr_id, zpr_nom, zpr_categorie,
-        zpr_date, zpr_duree, zpr_num_j, zpr_cmt, obr.obr_id, (obr.obr_nom || ' ' || obr.obr_prenom)
-        AS observateur, numerisateur, (num.obr_nom || ' ' || num.obr_prenom)
-        AS numerisat, coalesce(zpr_affectee, false) as zpr_affectee, cpt_enjeux,
-        cpt_station, cpt_lichen, cpt_flore, zpr_cibles, saisie.liste_cibles(zpr_cibles)
-        AS cibles FROM saisie.zone_prospection 
+    $req = "WITH cibles as (
+              SELECT string_agg(nom_complet, ' & ') AS cibles, zpr_cibles
+              FROM ( 
+                SELECT nom_complet, zpr_cibles
+                FROM inpn.v_cibles 
+                JOIN saisie.zone_prospection  
+                ON cd_nom = ANY(string_to_array(zpr_cibles, '&'))
+
+              ) a
+              GROUP BY nom_complet, zpr_cibles
+            )
+        SELECT st_asgeojson(st_transform(zpr_geom, 4326)), zpr_id, zpr_nom, zpr_categorie,
+          zpr_date, zpr_duree, zpr_num_j, zpr_cmt, obr.obr_id, (obr.obr_nom || ' ' || obr.obr_prenom)
+          AS observateur, numerisateur, (num.obr_nom || ' ' || num.obr_prenom)
+          AS numerisat, coalesce(zpr_affectee, false) as zpr_affectee, cpt_enjeux,
+          cpt_station, cpt_lichen, cpt_flore,
+          zp.zpr_cibles, cibles AS cibles 
+        FROM saisie.zone_prospection zp
         LEFT JOIN saisie.observateur obr USING(obr_id)
         LEFT JOIN saisie.numerisateur num ON numerisateur = num.obr_id
-        LEFT JOIN saisie.v_zpr_affectees USING(zpr_id) LEFT JOIN saisie.v_zpr_bilan
-        USING(zpr_id) WHERE " . $where . ' AND ' . $and . $orderLimit;
+        LEFT JOIN saisie.v_zpr_affectees USING(zpr_id) 
+        LEFT JOIN saisie.v_zpr_bilan USING(zpr_id) 
+        LEFT JOIN cibles c ON c.zpr_cibles = zp.zpr_cibles
+        WHERE " . $where . ' AND ' . $and . $orderLimit;
+
     $rs = $cnxPgBd->executeSql($req);
     $rsTot = $cnxPgBd->executeSql('SELECT COUNT(zpr_id) FROM saisie.zone_prospection
         LEFT JOIN saisie.observateur obr USING(obr_id)
@@ -28,6 +43,7 @@
     // cas particulier des géométries "NULL"
     $geomNull = '{"type": "MultiPolygon", "coordinates": []}'; // obligatoire pour SelectFeature (OpenLayers) et LegendPanel (GeoExt)
     $premiereFois = true;
+    
     while ($tab = pg_fetch_assoc($rs)) {
         $geom = $tab['st_asgeojson'];
         // nécessaire pour le FeatureReader.js bidouillé avec le rajout de la ligne "this.totalRecords = values.st_asgeojson"
