dda1af1c8ec693245fb124d70d9d3dabe8ec49f7
amandine-sahl <amandine.sahl@gmail.com>
2017-12-01 15:20:41 +0100

    Nettoyage et mise en exemple des fichiers de paramètres

--- Sources/Flore/Configuration/PostGreSQL.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //Paramètres fixes de connexion à la base PostGreSQL (SERVEUR PNC)
-    define('HOST', 'localhost');
-    define('PORT', '5432');
-    define('DBNAME', 'flore');
-    define('USER', 'obs_flore');
-    define('PASSWORD', 'obs_flore');
-    define('APPLI', 'ObsFlore');
-?>
